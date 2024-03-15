49fd03a9fd02785d518ce59fc887e6aa47314015
leotofoli <leoadt@gmail.com>
2024-03-14 22:56:59 -0300

    Configuração do JWT

---
+++ src/main/java/dio/diospringsecurityjwt/security/WebSecurityConfig.java
@@ -0,0 +1,60 @@
+package dio.diospringsecurityjwt.security;
+
+import jakarta.servlet.Servlet;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers((headers) -> headers.frameOptions(Customizer.withDefaults()).disable());
+        http.cors((cors) -> cors.disable());
+        http.csrf((csrf) -> csrf.disable());
+        http.authorizeHttpRequests(authz -> authz
+                .requestMatchers(SWAGGER_WHITELIST).permitAll()
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                .requestMatchers(HttpMethod.POST,"/users").permitAll()
+                .requestMatchers(HttpMethod.GET,"/users").hasAnyRole("USERS","MANAGERS")
+                .requestMatchers("/managers").hasAnyRole("MANAGERS")
+                .anyRequest().authenticated()).addFilterAfter(new JWTFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+    @Bean //HABILITANDO ACESSAR O H2-DATABSE NA WEB
+    public ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean((Servlet) new WebServlet());
+        registrationBean.addUrlMappings("/h2-console/*");
+        return registrationBean;
+    }
+}
